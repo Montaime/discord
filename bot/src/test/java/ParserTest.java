c905480229a0708c1eeb1ef108cdbad6d92b93d3
Azzerial <the.azzerial@icloud.com>
2020-12-25 17:08:11 +0000

    added basic tests for the Parser class

---
+++ bot/src/test/java/ParserTest.java
@@ -0,0 +1,34 @@
+import me.montai.discord.bot.utils.Parser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+
+    @Test
+    public void testIsNull() {
+        final String[] args = Parser.split(null);
+
+        Assertions.assertArrayEquals(args, new String[0]);
+    }
+
+    @Test
+    public void testIsEmpty() {
+        final String[] args = Parser.split("");
+
+        Assertions.assertArrayEquals(args, new String[0]);
+    }
+
+    @Test
+    public void testSimpleString() {
+        final String[] args = Parser.split("word1 word2 word3");
+
+        Assertions.assertArrayEquals(args, new String[] { "word1", "word2", "word3" });
+    }
+
+    @Test
+    public void testAdvancedString() {
+        final String[] args = Parser.split("word1 \"word2 word3\"");
+
+        Assertions.assertArrayEquals(args, new String[] { "word1", "word2 word3" });
+    }
+}
